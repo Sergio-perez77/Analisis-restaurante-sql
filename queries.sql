d7607e6e263e3fe40d097d634588857104b988a9
Sergio-perez77 <cuenta.respaldo777@outlook.com>
2026-03-25 16:50:20 -0300

    Update queries.sql
    pequeño cambio de espacios

--- queries.sql
+++ queries.sql
@@ -1,18 +1,18 @@
---promedio de cuenta
+-- promedio de cuenta
 select avg(total_bill) as promedio_cuenta from tips;
 
---promedio de propina
+-- promedio de propina
 select avg(tip) as promedio_propina from tips;
 
---ingresos por dia
+-- ingresos por dia
 select day, sum(total_bill) as ingresos from tips group by day order by ingresos desc;
 
---ingresos por horario
+-- ingresos por horario
 select time, sum(total_bill as ingresos from tips group by time;
 
---gasto por tamaño de grupo
+-- gasto por tamaño de grupo
 select size, avg(total_bill) as gasto_promedio group by size order by size;
 
---porcentaje de propina por dia
+-- porcentaje de propina por dia
 select day, avg(tip/total_bill) as porcentaje_propina from tips group by day order by porcentaje_propina;
 
